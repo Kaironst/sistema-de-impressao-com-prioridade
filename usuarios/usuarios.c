03bd05bfd88a908103bbf3fdf72173a12909180f
kaironst <pedronovak4@gmail.com>
2025-06-06 21:22:06 -0300

    - declaração de usuario movido para o local certo - inicializado usuarios.c

---
+++ usuarios/usuarios.c
@@ -0,0 +1,59 @@
+#include "usuarios.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+NoUsuario* iniListaUsuario() {
+    NoUsuario* header=(NoUsuario*)malloc(sizeof(NoUsuario));
+    header->nome="";
+    header->cpf=0;
+    header->tipoUsuario=ESTUDANTE;
+    header->proximo=NULL;
+    header->anterior=NULL;
+    return header;
+}
+
+int addUsuario(NoUsuario* header, char* nome, int cpf, tipoUsuario tipoUsuario) {
+    NoUsuario* novo=(NoUsuario*)malloc(sizeof(NoUsuario));
+    novo->nome=nome;
+    novo->cpf=cpf;
+    novo->tipoUsuario=tipoUsuario;
+    novo->proximo=header->proximo;
+    header->proximo=novo;
+    if (novo->proximo!=NULL)
+        novo->proximo->anterior=novo;
+    return 1;
+}
+
+int rmUsuario(NoUsuario* no) {
+    if (no==NULL || no->anterior==NULL) return 0;     // nó não encontrado
+    free(no->nome);
+    no->proximo->anterior=no->anterior;
+    no->anterior->proximo=no->proximo;
+    free(no);
+    no=NULL;
+    return 1;
+}
+
+NoUsuario* getUsuario(NoUsuario* header, char* nome) {
+    NoUsuario* atual=header->proximo;
+    while( atual!=NULL && strcmp(atual->nome,nome)!=0 ) {
+        atual=atual->proximo;
+    }
+    return atual;
+}
+
+NoUsuario* getUsuarioCpf(NoUsuario* header, int cpf) {
+    NoUsuario* atual=header->proximo;
+    while(atual!=NULL && atual->cpf!=cpf) {
+        atual=atual->proximo;
+    }
+    return atual;
+}
+
+int freeListaUsuario(NoUsuario* header) {
+    while(rmUsuario(header->proximo));
+    free(header);
+    header=NULL;
+    return 1;
+}
